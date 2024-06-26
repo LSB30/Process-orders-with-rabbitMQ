1e9d54e1cffe71f7f5e44a174f4ea1f8ab3bc02a
LSB30 <lukinhaasantana777@gmail.com>
2024-06-26 18:19:25 -0300

    Finish ✅

--- src/main/java/belato/lucas/orderms/controller/OrderController.java
+++ src/main/java/belato/lucas/orderms/controller/OrderController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class OrderController {
 
@@ -27,7 +29,10 @@ public class OrderController {
 
         var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page,pageSize));
 
+        var totalOnOnders = orderService.findTotalOrdersByCustomerId(customerId);
+
         return ResponseEntity.ok(new ApiResponse<>(
+                Map.of("totalOnOrders", totalOnOnders),
                 pageResponse.getContent(),
                 PaginationResponse.fromPage(pageResponse)
         ));
--- src/main/java/belato/lucas/orderms/service/OrderService.java
+++ src/main/java/belato/lucas/orderms/service/OrderService.java
@@ -5,19 +5,27 @@ import belato.lucas.orderms.dto.OrderResponse;
 import belato.lucas.orderms.entity.OrderEntity;
 import belato.lucas.orderms.entity.OrderItem;
 import belato.lucas.orderms.repository.OrderRepository;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -53,5 +61,16 @@ public class OrderService {
         var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
 
         return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 }
